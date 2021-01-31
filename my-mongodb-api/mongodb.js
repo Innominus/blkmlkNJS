5515da218784c60c024a97b0aa924b657910c2f4
Innominus <kadthekad@hotmail.com>
2021-01-31 21:54:46 +1100

    updated timeout

--- my-mongodb-api/mongodb.js
+++ my-mongodb-api/mongodb.js
@@ -34,8 +34,8 @@ export async function connectToDatabase() {
     const opts = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      socketTimeoutMS: 15000,
-      connectTimeoutMS: 15000,
+      socketTimeoutMS: 300000,
+      connectTimeoutMS: 300000,
     };
 
     cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
