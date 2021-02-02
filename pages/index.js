41c4ef1216805689612a325156db463a340bcae9
Innominus <kadthekad@hotmail.com>
2021-02-02 20:19:48 +1100

    CSS changes in hopes

--- pages/index.js
+++ pages/index.js
@@ -65,15 +65,15 @@ export default function App() {
   // Add UI feedback with modal
 
   return (
-    <div className="bg-blkCoffee bg-cover bg-center flex flex-col h-screen w-screen">
+    <div className=" bg-cover bg-center flex flex-col h-screen w-screen">
       <Head>
         <title>BLK MLK COVID-safety</title>
       </Head>
       {/* Banner */}
       <div className="bg-black h-auto flex items-center px-12 py-2 ">
-        <div className="flex gap-8">
+        <div className="flex ">
           <a href="https://www.facebook.com/blk.mlk.specialty.coffee">
-            <img className="h-16" src="/facebook.svg" />
+            <img className="h-16 pr-8" src="/facebook.svg" />
           </a>
           <a href="https://www.instagram.com/blk.mlk_specialty_coffee/?hl=en">
             <img className="h-16" src="/instagram.svg" />
@@ -83,92 +83,96 @@ export default function App() {
       </div>
 
       {/* Form */}
-      <div className="md:h-full  flex items-center justify-center">
-        <div className="max-w-2xl w-full bg-white shadow-lg p-8 ">
-          <h1 className="text-2xl font-medium mb-8">
-            Please enter your details
-          </h1>
-          <Formik
-            initialValues={{ regInputs: INITIAL_VALUES }}
-            onSubmit={(data, actions) => {
-              sendData(data, actions);
-              actions.resetForm();
-            }}
-            validationSchema={registerSchema}
-            validateOnChange={false}
-            validateOnBlur={false}
-          >
-            {({
-              values,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              errors,
-              isSubmitting,
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <FieldArray
-                  name="regInputs"
-                  validateOnChange={false}
-                  validateOnBlur={false}
-                  render={(arrayHelpers) => (
-                    <div className="flex flex-col text-left ">
-                      <div className="flex flex-col  max-h-full overflow-y-scroll">
-                        {values.regInputs.map((regInput, index) => (
-                          <RegistrationForm
-                            key={index}
-                            index={index}
-                            errors={errors}
-                            fvalue={regInput.First_Name}
-                            lvalue={regInput.Last_Name}
-                            phvalue={regInput.Ph_Number}
-                            handleBlur={handleBlur}
-                            handleChange={handleChange}
-                            fname={`regInputs[${index}].First_Name`}
-                            lname={`regInputs[${index}].Last_Name`}
-                            phNum={`regInputs[${index}].Ph_Number`}
-                          />
-                        ))}
-                      </div>
-                      <div className="flex pb-4">
-                        <button
-                          type="button"
-                          className="flex leading-7 mr-2 bg-black text-white w-14 h-8 rounded-md justify-center text-center hover:bg-gray-900 active:bg-gray-600 cursor-pointer"
-                          onClick={() => {
-                            arrayHelpers.push({
-                              regID: values.regInputs.length + 1,
-                              First_Name: "",
-                              Last_Name: "",
-                              Ph_Number: "",
-                            });
-                          }}
-                        >
-                          Add
-                        </button>
-                        <button
-                          type="button"
-                          className="flex leading-7 bg-black text-white w-20 h-8 rounded-md justify-center align-middle hover:bg-gray-900 active:bg-gray-600 cursor-pointer"
-                          onClick={() => {
-                            if (values.regInputs.length > 1) {
-                              arrayHelpers.remove(values.regInputs.length - 1);
-                            }
-                          }}
-                        >
-                          Remove
-                        </button>
-                      </div>
+      <div className="md:h-full  flex items-center justify-center bg-blkCoffee">
+        <div className="max-w-2xl w-full  bg-blkCoffee p-3">
+          <div className="max-w-2xl  w-full bg-white border border-black p-4 shadow-2xl">
+            <h1 className="text-2xl font-medium mb-8">
+              Please enter your details
+            </h1>
+            <Formik
+              initialValues={{ regInputs: INITIAL_VALUES }}
+              onSubmit={(data, actions) => {
+                sendData(data, actions);
+                actions.resetForm();
+              }}
+              validationSchema={registerSchema}
+              validateOnChange={false}
+              validateOnBlur={false}
+            >
+              {({
+                values,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                errors,
+                isSubmitting,
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <FieldArray
+                    name="regInputs"
+                    validateOnChange={false}
+                    validateOnBlur={false}
+                    render={(arrayHelpers) => (
+                      <div className="flex flex-col text-left ">
+                        <div className="flex flex-col  max-h-full md:overflow-y-scroll">
+                          {values.regInputs.map((regInput, index) => (
+                            <RegistrationForm
+                              key={index}
+                              index={index}
+                              errors={errors}
+                              fvalue={regInput.First_Name}
+                              lvalue={regInput.Last_Name}
+                              phvalue={regInput.Ph_Number}
+                              handleBlur={handleBlur}
+                              handleChange={handleChange}
+                              fname={`regInputs[${index}].First_Name`}
+                              lname={`regInputs[${index}].Last_Name`}
+                              phNum={`regInputs[${index}].Ph_Number`}
+                            />
+                          ))}
+                        </div>
+                        <div className="flex pb-4">
+                          <button
+                            type="button"
+                            className="flex leading-7 mr-2 bg-black text-white w-14 h-8 rounded-md justify-center text-center hover:bg-gray-900 active:bg-gray-600 cursor-pointer"
+                            onClick={() => {
+                              arrayHelpers.push({
+                                regID: values.regInputs.length + 1,
+                                First_Name: "",
+                                Last_Name: "",
+                                Ph_Number: "",
+                              });
+                            }}
+                          >
+                            Add
+                          </button>
+                          <button
+                            type="button"
+                            className="flex leading-7 bg-black text-white w-20 h-8 rounded-md justify-center align-middle hover:bg-gray-900 active:bg-gray-600 cursor-pointer"
+                            onClick={() => {
+                              if (values.regInputs.length > 1) {
+                                arrayHelpers.remove(
+                                  values.regInputs.length - 1
+                                );
+                              }
+                            }}
+                          >
+                            Remove
+                          </button>
+                        </div>
 
-                      <input
-                        className=" h-14 text-white bg-black font-medium shadow-lg hover:bg-gray-900 active:bg-gray-700 cursor-pointer"
-                        type="submit"
-                        disabled={isSubmitting}
-                      />
-                    </div>
-                  )}
-                />
-              </form>
-            )}
-          </Formik>
+                        <input
+                          className=" h-14 text-white bg-black font-medium shadow-lg hover:bg-gray-900 active:bg-gray-700 cursor-pointer"
+                          type="submit"
+                          disabled={isSubmitting}
+                        />
+                      </div>
+                    )}
+                  />
+                </form>
+              )}
+            </Formik>
+          </div>
         </div>
       </div>
     </div>
